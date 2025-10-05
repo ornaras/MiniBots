76f90ec76cd7eb090a370ced33037667920847f3
ornaras <ornaras.us@gmail.com>
2025-10-05 20:42:57 +0400

    Добавлен скрипт dinner.py

---
+++ scripts/dinner.py
@@ -0,0 +1,25 @@
+TG_TOKEN = ""
+VK_TOKEN = ""
+TG_CHAT = 0
+
+import requests
+import json
+
+vk_header = {
+    "Authorization": f"Bearer {VK_TOKEN}"
+}
+vk_url = "https://api.vk.ru/method/wall.get?domain=vremya_obedat18&count=1&v=5.199"
+vk_post = requests.get(vk_url, headers=vk_header)
+vk_json = json.loads(vk_post.content)
+attachments = vk_json["response"]["items"][0]["attachments"]
+
+tg_body = {
+    "chat_id": TG_CHAT,
+    "photo": ""
+}
+for attach in attachments:
+  if attach["type"] == "photo":
+    tg_body["photo"] = attach["photo"]["orig_photo"]["url"]
+    break
+tg_url = f"https://api.telegram.org/bot{TG_TOKEN}/sendPhoto"
+requests.post(tg_url, json=tg_body)
